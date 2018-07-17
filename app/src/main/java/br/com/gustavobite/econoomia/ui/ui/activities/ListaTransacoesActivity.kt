c466b0ac192fb60015bae65b04a4e09e26e893dc
Gustavo Bitencourt <gustavobite@gmail.com>
2018-07-17 00:34:43 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/gustavobite/econoomia/ui/ui/activities/ListaTransacoesActivity.kt
@@ -0,0 +1,18 @@
+package br.com.gustavobite.econoomia.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    /**
+     * override - é similar ao Java. No kotlin exige usarmos quando há uma sobreescrita de um método.
+     * fun - por usar de ambos paradigmas (POO e funcional), usa-se funções (fun)
+     * Quando não for definido nenhum retorno, por padrão é retornado um Unit
+     * Unit é similar ao Void no Java
+     * Nos parâmetros, utiliza-se notação Pascal
+     */
+    override fun onCreate(savedInstanceState: Bundle?) : Unit {
+        super.onCreate(savedInstanceState)
+    }
+}
